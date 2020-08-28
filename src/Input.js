f1f61a9db5d1942dbb6126e392463a1f87e64945
Gabriel Polveiro <gabriel-polveiro@hotmail.com>
2020-08-28 15:15:29 -0300

    Input component refactor: receiveing props

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 // stateless component
-function Input() {
-    return <input type="text" placeholder="Digite um todo" />
+function Input(props) {
+   return (
+    <input 
+     type="text" 
+     placeholder="Digite um todo"
+     onChange={e => props.onChange(e)}
+    />
+    )
 }
 
 export default Input;
